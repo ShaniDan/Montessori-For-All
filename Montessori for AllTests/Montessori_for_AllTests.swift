2c49f512a541c9abd95672e43c644e3d78d05fbc
Shani <106402127+ShaniDan@users.noreply.github.com>
2025-11-17 12:13:06 -0800

    initial commit

---
+++ Montessori for AllTests/Montessori_for_AllTests.swift
@@ -0,0 +1,16 @@
+//
+//  Montessori_for_AllTests.swift
+//  Montessori for AllTests
+//
+//  Created by Shakhnoza Mirabzalova on 11/17/25.
+//
+
+import Testing
+
+struct Montessori_for_AllTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
