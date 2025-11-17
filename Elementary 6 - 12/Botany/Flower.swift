ce3dba1a92e610dc9d482b0f289b16a3c4392390
Shani <106402127+ShaniDan@users.noreply.github.com>
2025-11-17 13:21:45 -0800

    initial changes-structure

---
+++ Elementary 6 - 12/Botany/Flower.swift
@@ -0,0 +1,14 @@
+//
+//  Flower.swift
+//  Montessori for All
+//
+//  Created by Shakhnoza Mirabzalova on 11/17/25.
+//
+
+import Foundation
+
+//https://www.nienhuis.com/eu/product/botany-puzzle-activity-set/400_020201/
+/*
+ I need to follow this material to build a small AR MVP that detects a flower using the device camera and overlays its anatomical parts—such as the pistil, stamen, sepal, and others—in real time. When the camera recognizes the flower, the app should identify the main structures and display them as AR labels or annotations. https://theglobalmontessorinetwork.org/resource/elementary/parts-of-flower-english/
+ */
+
